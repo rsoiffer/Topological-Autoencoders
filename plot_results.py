ddc361df101dc99738b4a788d222cd10c0543c60
Weirdbob95 <rory.soiffer@comcast.net>
2018-12-13 00:01:52 -0800

    Cleaned up code for github

--- plot_results.py
+++ plot_results.py
@@ -4,26 +4,29 @@ from algorithm import *
 m = 2
 
 # train_points, train_labels, test_points, test_labels = datasets.subset(datasets.cifar10(), 5000, 1000)
-train_points, train_labels, test_points, test_labels = datasets.subset(datasets.cifarX(2), 5000, 1000)
-# train_points, train_labels, test_points, test_labels = datasets.subset(datasets.mnist10(), 5000, 1000)
+# train_points, train_labels, test_points, test_labels = datasets.subset(datasets.cifarX(2), 5000, 1000)
+train_points, train_labels, test_points, test_labels = datasets.subset(datasets.mnist10(), 5000, 1000)
 print('Loaded input')
 # print('Raw nearest-neighbor accuracy:', nn_accuracy(train_points, train_labels, test_points, test_labels))
-
-train_points_1, test_points_1 = pca(train_points, m, test_points)
-print('PCA nearest-neighbor accuracy:', nn_accuracy(train_points_1, train_labels, test_points_1, test_labels))
 
 train_points_2, test_points_2, graphs = algorithm(
     train_points,
     m=m,
     test_points=test_points,
     pca_dim=100,
-    # network_depth=5,
     loss_weights=(1, 1, 1),
-    # intermediate_dims=(10,),
 )
 print(nn_accuracy(train_points_2, train_labels, test_points_2, test_labels))
 
-# for i in range(3):
+plt_plot(train_points_2, train_labels)
+plt.plot(graphs)
+plt.show()
+
+
+# train_points_1, test_points_1 = pca(train_points, m, test_points)
+# print('PCA nearest-neighbor accuracy:', nn_accuracy(train_points_1, train_labels, test_points_1, test_labels))
+#
+# for i in range(5):
 #     train_points_2, test_points_2, graphs = algorithm(
 #         train_points,
 #         m=m,
@@ -32,10 +35,10 @@ print(nn_accuracy(train_points_2, train_labels, test_points_2, test_labels))
 #         loss_weights=(1, 1, 1),
 #         intermediate_dims=(10,),
 #     )
-#     with open('results/tenc_4P_mnist.txt', 'a') as f:
+#     with open('results/tenc_mytest.txt', 'a') as f:
 #         print(nn_accuracy(train_points_2, train_labels, test_points_2, test_labels), end=',', file=f)
 #
-# for i in range(3):
+# for i in range(5):
 #     train_points_2, test_points_2, graphs = algorithm(
 #         train_points,
 #         m=m,
@@ -44,10 +47,10 @@ print(nn_accuracy(train_points_2, train_labels, test_points_2, test_labels))
 #         loss_weights=(1, 0, 1),
 #         intermediate_dims=(10,),
 #     )
-#     with open('results/tsne_4P_mnist.txt', 'a') as f:
+#     with open('results/tsne_mytest.txt', 'a') as f:
 #         print(nn_accuracy(train_points_2, train_labels, test_points_2, test_labels), end=',', file=f)
 #
-# for i in range(3):
+# for i in range(5):
 #     train_points_2, test_points_2, graphs = algorithm(
 #         train_points,
 #         m=m,
@@ -56,10 +59,5 @@ print(nn_accuracy(train_points_2, train_labels, test_points_2, test_labels))
 #         loss_weights=(0, 1, 1),
 #         intermediate_dims=(10,),
 #     )
-#     with open('results/auto_4P_mnist.txt', 'a') as f:
+#     with open('results/auto_mytest.txt', 'a') as f:
 #         print(nn_accuracy(train_points_2, train_labels, test_points_2, test_labels), end=',', file=f)
-
-
-plt_plot(train_points_2, train_labels)
-plt.plot(graphs)
-plt.show()
